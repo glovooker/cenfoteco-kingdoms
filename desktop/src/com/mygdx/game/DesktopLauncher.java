bf08710b0ebbc29d092e5e3c0229df8e65fa843f
Franklin <fcastror@ucenfotec.ac.cr>
2023-03-18 17:06:56 -0600

    Set window icon

--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
@@ -10,6 +11,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("DungeosAndArmy");
+		config.setWindowIcon(Files.FileType.Internal, "icons/Diamond_Sword.png");
 		new Lwjgl3Application(new MyGdxGame(), config);
 	}
 }
