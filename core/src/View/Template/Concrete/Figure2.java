69ca2eba1e07598f48c6ebf6d7ecde9b52484281
Jocselyn Aguilar <ajocseline@gmail.com>
2023-04-10 21:08:42 -0600

    figure2 ready

--- core/src/View/Template/Concrete/Figure2.java
+++ core/src/View/Template/Concrete/Figure2.java
@@ -7,6 +7,7 @@ import View.BoardView;
 import View.Template.Figure;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Figure2 extends Figure {
@@ -17,27 +18,95 @@ public class Figure2 extends Figure {
 
     @Override
     protected List<Coordinate> filterInvalidCoordinates(List<Coordinate> coordinates, Direction direction) {
-        return coordinates;
+        List<Coordinate> result = new ArrayList<>();
+
+        for (Coordinate coordinate : coordinates){
+
+            if(!(hasAnyCollision(coordinate, direction))){
+                result.add(coordinate);
+            }
+
+        }
+        return result;
     }
 
     @Override
     protected boolean hasAnyCollision(Coordinate coordinate, Direction direction) {
+        int contX = 0;
+
+        if(validateEdges(coordinate, direction)){
+            return true;
+        }
+
+        TileActor nextTile = BoardView.getTileByPosition(coordinate.getX() + 1, coordinate.getY());
+
+        int initialX = nextTile.isBusy() ?  coordinate.getX() - 1 : coordinate.getX();
+        int currentY = coordinate.getY();
+
+        while (contX < 3){
+            int currentX =  initialX + contX;
+
+            TileActor tile = BoardView.getTileByPosition(currentX, currentY);
+
+            if(tile.isBusy()){
+                return true;
+            }
+
+            if(contX == 1) {
+                if(hasAnyCollisionOnY(currentX, coordinate.getY(), direction)) {
+                    return true;
+                }
+
+                currentY += 2;
+            }
+
+            contX ++;
+        }
+
         return false;
+    }
+
+    private  boolean hasAnyCollisionOnY(int x, int y, Direction direction) {
+        int contY = 1;
+
+        while(contY < 2) {
+            int currentY = direction == Direction.Up ? y + contY : y - contY;
+            TileActor tile = BoardView.getTileByPosition(x, currentY);
+
+            if(tile.isBusy()){
+                return true;
+            }
+
+            contY++;
+        }
+
+        return false;
+    }
+
+    private boolean validateEdges(Coordinate coordinate, Direction direction){
+        int newY = direction == Direction.Down ? coordinate.getY() - 2 : coordinate.getY() + 2;
+
+        if(newY <= 0 || newY > BoardView.getRowsInBoard()){
+            return true;
+        }
+
+        return coordinate.getX() + 2 > BoardView.getRowsInBoard() - 1 || coordinate.getX() <= 0;
     }
 
     @Override
     protected void paintingWayUp(Coordinate coordinate) {
         int blockNumberToPaint = Figure.AMOUNT_BLOCKS;
         TileActor tile;
-        if(coordinate.getX() <= 0) {
+
+        if(coordinate.getX() <= 0 || coordinate.getX() >= BoardView.getRowsInBoard() - 3) {
             // TODO: Tumba que tumba que tumba tumba tumba
             return;
         }
 
-        TileActor prevTile = BoardView.getTileByPosition(coordinate.getX(), coordinate.getY());
+        TileActor nextTile = BoardView.getTileByPosition(coordinate.getX() + 1, coordinate.getY());
 
-        if(prevTile.isBusy()) {
-            coordinate.setX(coordinate.getX() + 1);
+        if(nextTile.isBusy()) {
+            coordinate.setX(coordinate.getX() - 1);
         }
 
         while(!(blockNumberToPaint == Figure.AMOUNT_BLOCKS - 3 )){
@@ -77,15 +146,15 @@ public class Figure2 extends Figure {
         int blockNumberToPaint = Figure.AMOUNT_BLOCKS;
         TileActor tile;
 
-        if(coordinate.getX() <= 0) {
+        if(coordinate.getX() <= 0 || coordinate.getX() >= BoardView.getRowsInBoard() - 3) {
             // TODO: Tumba que tumba que tumba tumba tumba
             return;
         }
 
-        TileActor prevTile = BoardView.getTileByPosition(coordinate.getX(), coordinate.getY());
+        TileActor nextTile = BoardView.getTileByPosition(coordinate.getX() + 1, coordinate.getY());
 
-        if(prevTile.isBusy()) {
-            coordinate.setX(coordinate.getX() + 1);
+        if(nextTile.isBusy()) {
+            coordinate.setX(coordinate.getX() - 1);
         }
 
         while(blockNumberToPaint != (Figure.AMOUNT_BLOCKS - 3)){
